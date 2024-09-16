d4a34730042cb572a5144662bac24f7c7a60cfa9
Michał Kurczak <kurczakmichal97@gmail.com>
2024-09-16 06:50:03 +0200

    chore: include basic theme setup

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -4,21 +4,31 @@ import 'package:flutter/material.dart';
 import 'package:hack_yeah_2k24/app/router/navigation_observer.dart';
 import 'package:hack_yeah_2k24/app/router/router.dart';
 import 'package:hack_yeah_2k24/l10n/l10n.dart';
+import 'package:hack_yeah_2k24/presentation/theme/app_typography.dart';
+import 'package:hack_yeah_2k24/presentation/theme/color_palette.dart';
+import 'package:hack_yeah_2k24/presentation/theme/theme_helpers.dart';
 
 class App extends StatelessWidget {
   App({super.key});
 
   final _appRouter = AppRouter();
 
+  final ligthTheme = ThemeDataExtensions.createThemeData(
+    palette: ColorPalette.lightPalette,
+    typography: AppTypography.defaultTypography,
+    brightness: Brightness.light,
+  );
+
+  final darkTheme = ThemeDataExtensions.createThemeData(
+    palette: ColorPalette.darkPalette,
+    typography: AppTypography.defaultTypography,
+    brightness: Brightness.dark,
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
-        useMaterial3: true,
-      ),
+      theme: ligthTheme,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       routerConfig: _appRouter.config(
