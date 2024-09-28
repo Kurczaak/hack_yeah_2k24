9f338ebfaffe5543708bb6111cae6fd8f425a958
Kurczaak <kurczakmichal97@gmail.com>
2024-09-28 23:06:26 +0200

    feat: add places repo

--- lib/data/config/config.dart
+++
@@ -1,3 +0,0 @@
-abstract class Config {
-  static const baseUrl = 'https://hackyea2024.vercel.app';
-}
---
+++ lib/domain/repositories/places_repo.dart
@@ -0,0 +1,16 @@
+import 'package:hack_yeah_2k24/data/data_source/places_data_source.dart';
+import 'package:hack_yeah_2k24/domain/model/place_prediction.dart';
+import 'package:hack_yeah_2k24/domain/model/place_prediction_request.dart';
+import 'package:injectable/injectable.dart';
+
+@injectable
+class PlacesRepo {
+  final PlacesDataSource dataSource;
+
+  PlacesRepo({required this.dataSource});
+  Future<List<PlacePrediction>> getPlacePredictions(
+      PlacePredictionRequest request) async {
+    final result = await dataSource.getPlacePredictions(request);
+    return result.toDomain();
+  }
+}
