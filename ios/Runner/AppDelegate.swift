8cdd8f30d569160eb32d0bc8183d6019d05de6c4
Kurczaak <kurczakmichal97@gmail.com>
2024-09-29 06:28:43 +0200

    chore: hide api keys

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,14 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDQOh7v_Y0zz64NKkVGvJFV03gbFaV8ZwE")
+    let dartDefinesString = Bundle.main.infoDictionary!["DART_DEFINES"] as! String
+    var dartDefinesDictionary = [String:String]()
+    for definedValue in dartDefinesString.components(separatedBy: ",") {
+    let decoded = String(data: Data(base64Encoded: definedValue)!, encoding: .utf8)!
+    let values = decoded.components(separatedBy: "=")
+    dartDefinesDictionary[values[0]] = values[1]
+    }
+    GMSServices.provideAPIKey(dartDefinesDictionary["GOOGLE_MAPS_API_KEY"]!)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
